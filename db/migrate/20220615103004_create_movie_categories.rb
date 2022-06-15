1168a09ff221bd64fa1f16cab2f487c0de61f7cc
savka-olia <tokarchykolia@gmail.com>
2022-06-15 13:49:26 +0300

    add many-to-many association

---
+++ db/migrate/20220615103004_create_movie_categories.rb
@@ -0,0 +1,9 @@
+class CreateMovieCategories < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movie_categories do |t|
+      t.integer :movie_id
+      t.integer :category_id
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_14_104325) do
+ActiveRecord::Schema.define(version: 2022_06_15_103004) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2022_06_14_104325) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "movie_categories", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
